eb64177d9cc7c58c257a88f5ef9a2bfe97fec1e2
samis <samilsully@gmail.com>
2020-11-04 14:12:34 -0800

    adding in footer

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Portfolio from "./components/pages/Portfolio";
 import Errorpage from "./components/pages/Errorpage";
+import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/contact" component={Contact} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route path="/" component={Errorpage} />
-        </Switch>
-      </Router>
+      <div>
+        <Navbar />
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route path="/" component={Errorpage} />
+          </Switch>
+        </Router>
+      </div>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+            
+  <span class="navbar-brand mb-0 h1">Footer</span>
+
+    );
+}
+
+export default Footer;
